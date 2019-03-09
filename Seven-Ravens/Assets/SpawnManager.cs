21854999ee7659e9fdc425988e617ea027bf7e6b
Ori Lazar <36171247+OriLazar@users.noreply.github.com>
2019-03-09 14:43:03 +0000

    Dynamic spawning + quality of life editor script initialized

---
+++ Seven-Ravens/Assets/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Rokemon {
+    public class SpawnManager : MonoBehaviour
+    {
+        // reference to player prefab
+        [SerializeField]
+        private GameObject _playerPrefab;
+
+        private void Start()
+        {
+            if (PlayerController.Instance == null)
+            {
+                GameObject dynamicParent = GameObject.FindGameObjectWithTag("DynamicParent");
+                GameObject spawnPoint = GameObject.FindGameObjectWithTag("PlayerSpawn");
+
+                Instantiate(_playerPrefab, spawnPoint.transform.position, Quaternion.identity, dynamicParent.transform);
+            }
+        }
+    }
+}
