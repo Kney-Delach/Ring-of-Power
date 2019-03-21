ca44c0f3bf7e6bbc18678b7912af48067f38d815
Ori Lazar <36171247+OriLazar@users.noreply.github.com>
2019-03-21 13:00:38 +0000

    Invisibility functional

--- Seven-Ravens/Assets/Scripts/Player/PlayerController.cs
+++ Seven-Ravens/Assets/Scripts/Player/PlayerController.cs
@@ -190,6 +190,12 @@ namespace Rokemon
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, _leftBoundary.x, _rightBoundary.x), Mathf.Clamp(transform.position.y, _leftBoundary.y, _rightBoundary.y), transform.position.z);
         }
         
+        // sets player controller gameobject position
+        public void SetPosition(Transform newPosition)
+        {
+            GetComponent<Transform>().position = new Vector3(newPosition.position.x, newPosition.position.y, newPosition.position.z);
+        }
+
         // set background map bounds of current level, stops player character from walking out of bounds
         public void SetBounds()
         {
@@ -245,6 +251,7 @@ namespace Rokemon
             }
             if(Input.GetKeyDown(KeyCode.T))
             {
+                CastSpell("Invisibility");  // cast invisibility
                 Debug.Log("Pressed Key: T");
             }
             if(Input.GetKeyDown(KeyCode.F))
@@ -276,17 +283,24 @@ namespace Rokemon
             _speed /=2;
 
         }
+        private IEnumerator InvisibleCoroutine(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+            GetComponent<SpriteRenderer>().color = Color.white; 
+        }
 
         // TODO : Add checks for reload 
         // function casting a spell
         public void CastSpell(string spellName)
         {
-            GameObject ability = null; 
-
-            switch (spellName)
+            if(_abilitiesDatabase[spellName]._active && _mana.CurrentValue >= _abilitiesDatabase[spellName]._cost)
             {
-                case "Firebolt":
-                    if(_abilitiesDatabase[spellName]._active && _mana.CurrentValue >= _abilitiesDatabase[spellName]._cost)
+                GameObject ability = null; 
+
+                switch (spellName)
+                {
+                    case "Firebolt":
+                        // if(_abilitiesDatabase[spellName]._active && _mana.CurrentValue >= _abilitiesDatabase[spellName]._cost)
                         ability = (GameObject)Instantiate(_abilitiesDatabase[spellName]._prefab, transform.position, Quaternion.identity);
                         if( ability != null)
                         {
@@ -295,39 +309,34 @@ namespace Rokemon
                             fireboltController._target = _currentTarget;
                             fireboltController.Damage = _abilitiesDatabase[spellName]._damage;
                         }
-                    Debug.Log("Casting Spell: " + spellName);
-                    break;
-                case "Invisibility":
-                    if(_abilitiesDatabase[spellName]._active)
                         Debug.Log("Casting Spell: " + spellName);
-                    break;
-                case "Haste":
-                    if(_abilitiesDatabase[spellName]._active && _mana.CurrentValue >= _abilitiesDatabase[spellName]._cost)
+                        break;
+                    case "Invisibility":
+                        GetComponent<SpriteRenderer>().color = Color.blue; 
+                        // TODO: Implement invisibility 
+                        UseMana(_abilitiesDatabase[spellName]._cost);
+                        StartCoroutine(InvisibleCoroutine(_abilitiesDatabase[spellName]._damage));
+                        Debug.Log("Casting Spell: " + spellName);
+                        break;
+                    case "Haste":
                         UseMana(_abilitiesDatabase[spellName]._cost);
                         _speed *= 2; 
                         StartCoroutine(HasteCoroutine(_abilitiesDatabase[spellName]._damage));
                         Debug.Log("Casting Spell: " + spellName);
-                    break;
-                case "ProtectiveBubble":
-                    if(_abilitiesDatabase[spellName]._active)
+                        break;
+                    case "ProtectiveBubble":
                         Debug.Log("Casting Spell: " + spellName);
-                    break;
-                case "RemoveRoots":
-                    if(_abilitiesDatabase[spellName]._active)
+                        break;
+                    case "RemoveRoots":
                         Debug.Log("Casting Spell: " + spellName);
-                    break;
-                case "WaterFreeze":
-                    if(_abilitiesDatabase[spellName]._active)
+                        break;
+                    case "WaterFreeze":
                         Debug.Log("Casting Spell: " + spellName);
-                    break;
-                case "Polymorph":
-                    if(_abilitiesDatabase[spellName]._active)
+                        break;
+                    case "Polymorph":
                         Debug.Log("Casting Spell: " + spellName);
-                    break;
-                case "Heal":
-                    if(_abilitiesDatabase[spellName]._active && _mana.CurrentValue >= _abilitiesDatabase[spellName]._cost)
-                    {
-                        Debug.Log("Casting Spell: " + spellName);
+                        break;
+                    case "Heal":
                         if(_currentTarget != null && _currentTarget.tag == "HealableEnemy" && (_currentTarget.gameObject.GetComponent<Stats>().CurrentValue != _currentTarget.gameObject.GetComponent<Stats>().MaxValue) )
                         {
                             UseMana(_abilitiesDatabase[spellName]._cost);
@@ -338,15 +347,17 @@ namespace Rokemon
                             UseMana(_abilitiesDatabase[spellName]._cost);
                             _health.AddValue(_abilitiesDatabase[spellName]._damage);                       
                         }
-                    }
-                    else 
-                    {
-                        Debug.Log("PlayerController CastSpell: Ability inactive - Heal cannot be cast");
-                    }
-                    break;
-                default:
-                    break;
+                        Debug.Log("Casting Spell: " + spellName);
+                        break;
+                    default:
+                        break;
+                }
             }
+            else 
+            {
+
+            }
+           
         }
 
         // reduce mana value
